51a9d9905988f665cddc69508b5085f03f04d051
mbuchwald <m.e.buchwald@gmail.com>
2016-08-31 18:28:45 -0300

    Fine Tuned MLP, before starting with feature engineering

--- prediction.py
+++ prediction.py
@@ -1,7 +1,8 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
-from keras.regularizers import l1
+from keras.regularizers import l2
+from keras.optimizers import SGD
 
 def mlp(dim):
 	model = Sequential()
@@ -13,27 +14,20 @@ def mlp(dim):
 	#model.add(Dropout(0.5))
 	model.add(Dense(1, init='normal', activation='sigmoid'))
 
+	sgd = SGD(lr=0.1, decay=1e-7, momentum=0.0, nesterov=True)
 	model.compile(#loss='binary_crossentropy',
 			loss='mean_squared_error',
-              optimizer='rmsprop',
+              optimizer=sgd, #'rmsprop',
               metrics=['accuracy'])
 
 	return model
-
-def regression(dim):
-	# create model
-	model = Sequential()
-	model.add(Dense(1024, input_dim=dim, init='normal', activation='relu'))	
-	model.add(Dense(256, input_dim=dim, init='normal', activation='relu'))
-	model.add(Dense(1, init='normal'))
-	# Compile model
-	model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['accuracy'])
-	return model
+	
 
 def regression_with_regularization(dim):
+	# Usar L2
 	# create model
 	model = Sequential()
-	model.add(Dense(1024, input_dim=dim, init='normal', activation='relu', W_regularizer=l1(0.0001)))	
+	model.add(Dense(1024, input_dim=dim, init='normal', activation='relu', W_regularizer=l2(0.0001)))	
 	model.add(Dense(512, input_dim=dim, init='normal', activation='relu'))
 	model.add(Dense(1, init='normal'))
 	# Compile model
@@ -41,13 +35,13 @@ def regression_with_regularization(dim):
 	return model
 
 
-def train(words, predictions, iterations = 100):
+def train(words, predictions, iterations = 500):
 
 	model = mlp(words.shape[1])
 	predictions = (predictions.astype('float') - 1.0)/ 4.0
 
 	history = model.fit(words, predictions,
-                    nb_epoch=iterations, batch_size=1000,
-                    verbose=2, validation_split=0.2)
+                    nb_epoch=iterations, batch_size=5000,
+                    verbose=2, validation_split=0.1, shuffle=True)
 	
 	return model
